cc9d11a7df7fb6a752db17fd9df7a780b0348709
abelfubu <abelfubu@gmail.com>
2021-06-27 14:40:04 +0200

    :sparkles: chore(init): Initial Commit

---
+++ container/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MarketingApp from './components/MarkegingApp'
+
+const App = () => <div>
+  <h1>Hi there</h1>
+  <hr />
+  <MarketingApp />
+</div>
+
+export default App
